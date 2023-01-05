2ecadd2466a92b8c5bcba4f7b1e46c728966e61d
Ngoc Ba <bangoc.gdg@gmail.com>
2023-01-05 08:03:03 +0700

    add pipeline

--- generate_keywords.py
+++ generate_keywords.py
@@ -4,26 +4,35 @@ from sentence_transformers import SentenceTransformer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-n_gram_range = (1, 1)
-stop_words = "english"
+stop_words = []
+with open('vietnamese-stopwords.txt', encoding='utf8') as f:
+      for line in f:
+            stop_words.append(line.strip())
+
+doc = ''
+with open('input_line.txt', encoding='utf8') as f:
+      doc = f.read()
 
 
-# Automatically download VnCoreNLP components from the original repository
-# and save them in some local machine folder
+def removeStopWords(o_sen):
+      words = [word for word in o_sen.split() if word not in stop_words]
+      return " ".join(words)
+
+n_gram_range = (2, 2)
+
 py_vncorenlp.download_model(save_dir=os.path.abspath('./vncorenlp'))
 
 # Load the word and sentence segmentation component
 rdrsegmenter = py_vncorenlp.VnCoreNLP(annotators=["wseg"], save_dir=os.path.abspath('./vncorenlp'))
 
-doc = """
-         Bánh cheesecake ngọt mềm cực kỳ hấp dẫn và kích thích vị giác. Cùng học cách làm bánh cheesecake việt quất béo ngậy thơm ngon dễ làm tại nhà ngay nhé
-      """
-
 doc_segmented = rdrsegmenter.word_segment(doc)
 # Extract candidate words/phrases
-count = CountVectorizer(ngram_range=n_gram_range, stop_words=stop_words).fit([doc])
-candidates = count.get_feature_names()
 
+candidates = []
+for r in [(1, 1), (2, 2)]:
+      count = CountVectorizer(ngram_range=r).fit([removeStopWords(doc)])
+      nr_candidates = count.get_feature_names()
+      candidates.extend(nr_candidates)
 
 model = SentenceTransformer('distiluse-base-multilingual-cased-v2')
 
@@ -31,7 +40,7 @@ doc_embedding = model.encode([doc])
 candidate_embeddings = model.encode(candidates)
 
 
-top_n = 10
+top_n = 20
 distances = cosine_similarity(doc_embedding, candidate_embeddings)
 keywords = [candidates[index] for index in distances.argsort()[0][-top_n:]]
 
